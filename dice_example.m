4d333de28dd4b32d01641ed7b53a1bd91b4ba80f
g-pica <giuseppe.pica@iit.it>
2017-04-04 18:28:09 +0200

    Fixed uninteresting details to match the paper definitions of the dice example

--- dice_example.m
+++ dice_example.m
@@ -1,10 +1,12 @@
 function dice_example
 
+tic
 
 aa=1;
 lambdastep=.05;
-lambda=0;
+lambda=0.;
 
+accuracy=0.01;%with glpk, setting accuracy to 0.001 doesn't improve things, which are already good enough
 
 I_shar=zeros(1,ceil(1/lambdastep)+1);
 I_unx=zeros(1,ceil(1/lambdastep)+1);
@@ -56,7 +58,7 @@ while aa<=ceil(1/lambdastep)+1 % loop over different correlations between the di
        for yy=1:dimy
            for zz=1:dimz
 
-              if xx==ceil(zz+alpha*yy)-alpha
+              if xx==ceil(alpha*zz+yy)-alpha
                   p_x_yz(xx,yy,zz)=1;
               end
 
@@ -79,18 +81,30 @@ while aa<=ceil(1/lambdastep)+1 % loop over different correlations between the di
 
     p=p/sum(sum(sum(p)));%final normalization
 
-     
-    [I_shar(aa),I_syn(aa),I_unx(aa),I_uny(aa),q_1]=PID_code(dimx,dimy,dimz,p,0.001,'linprog');
-    %or use 'cvx' as the solving method
-
+    p=permute(p,[3 2 1]);
+    
+    %[I_shar(aa),I_syn(aa),I_unx(aa),I_uny(aa),q_1]=PID_code(dimz,dimy,dimx,p,accuracy,'linprog',10^-6);
+    
+    %[I_shar(aa),I_syn(aa),I_unx(aa),I_uny(aa),q_1]=PID_code(dimz,dimy,dimx,p,0.01,'cvx',10^-3);
+    
+    [I_shar(aa),I_syn(aa),I_unx(aa),I_uny(aa),q_1]=PID_code(dimz,dimy,dimx,p,accuracy,'glpk',0);
+    
+    %[I_shar(aa),I_syn(aa),I_unx(aa),I_uny(aa),q_1]=PID_code(dimz,dimy,dimx,p,accuracy,'lpsolve',0);
+    
 toc
 
     p_z=permute(p,[3 2 1]);
 
-    [I_shar_z(aa),I_syn_z(aa),I_unx_z(aa),I_uny_z(aa),q_3]=PID_code(dimx_z,dimy_z,dimz_z,p_z,0.001,'linprog');
-    %or use 'cvx' as the solving method
+   %[I_shar_z(aa),I_syn_z(aa),I_unx_z(aa),I_uny_z(aa),q_3]=PID_code(dimz_z,dimy_z,dimx_z,p_z,accuracy,'linprog',10^-6);
+   
+   %[I_shar_z(aa),I_syn_z(aa),I_unx_z(aa),I_uny_z(aa),q_3]=PID_code(dimz_z,dimy_z,dimx_z,p_z,0.01,'cvx',10^-3);
+        %or use 'cvx' as the solving method
 
-
+   [I_shar_z(aa),I_syn_z(aa),I_unx_z(aa),I_uny_z(aa),q_3]=PID_code(dimz_z,dimy_z,dimx_z,p_z,accuracy,'glpk',0.);
+    
+    %[I_shar_z(aa),I_syn_z(aa),I_unx_z(aa),I_uny_z(aa),q_3]=PID_code(dimz_z,dimy_z,dimx_z,p_z,accuracy,'lpsolve',0.);
+   
+        
     RSI_x_y_z(aa)=min(I_shar(aa),I_shar_z(aa));% in the draft paper, this is RSI between X and Z passing through Y
     IRSI_x_y_z(aa)=I_shar(aa)-RSI_x_y_z(aa);% this is the IRSI between X and Z passing through Y
 
@@ -101,6 +115,7 @@ toc
 
 end
 
+toc
 
 end
 
