3b509eab7ae32ed09c2b516f1fc7cc0fb4cae034
Eugenio Piasini <eugenio.piasini@iit.it>
2017-04-06 14:45:16 +0200

    Various bugfixes - still not working properly.

--- information_decomposition_linearized.py
+++ information_decomposition_linearized.py
@@ -1,5 +1,5 @@
 import numpy as np
-import glpk
+import cvxpy
 
 def I_2D(q_xy):
     """Compute mutual information I(X:Y) from Q(X,Y), where Q must be
@@ -22,14 +22,15 @@ def I_3D_cond_z(q_xyz):
     q_z = q_xyz.sum(axis=(1,2), keepdims=True)
     q_xz = q_xyz.sum(axis=1, keepdims=True)
     q_yz = q_xyz.sum(axis=0, keepdims=True)
-    q_x_z = q_xz / q_z
-    q_y_z = q_yz / q_z
-    q_xy_z = q_xyz / q_z
     with np.errstate(divide='ignore', invalid='ignore'):
+        q_x_z = q_xz / q_z
+        q_y_z = q_yz / q_z
+        q_xy_z = q_xyz / q_z
         table = q_xyz * np.log2(q_xy_z/(q_x_z*q_y_z))
-    return table[table>0].sum()    
+    table[np.logical_not(np.isfinite(table))] = 0
+    return table.sum()    
 
-def lin_pid(p, accuracy=0.01, method='glpk'):
+def lin_pid(p, accuracy=0.01, method='glpk', verbose=False):
 
     # work out dimensionality of the input probability distribution
     (dimx, dimy, dimz) = p.shape
@@ -57,36 +58,38 @@ def lin_pid(p, accuracy=0.01, method='glpk'):
 
     # Coefficients of the matrix q expressed in the basis of Gamma
     # matrices
-    parameters = np.zeros(dimz*(dimx-1)*(dimy-1))
+    parameters = np.zeros((dimz*(dimx-1)*(dimy-1), 1))
 
     # starting point of the algorithm
     q = p[:]
     coeff_prev = parameters[:]
 
+    
+
  
     while not done:
         # compute derivative of MI_q along the basis vectors
-        deriv = np.log2(q[:-1,:-1]*q[1:,1:]) - \
-                np.log2(q[:-1,1:]*q[1:,:-1]) + \
-                np.log2(q[:-1,1:].sum(axis=2, keepdims=True) * \
-                        q[1:,:-1].sum(axis=2, keepdims=True)) - \
-                np.log2(q[:-1,:-1].sum(axis=2, keepdims=True) * \
-                        q[1:,1:].sum(axis=2, keepdims=True))
+        with np.errstate(divide='ignore', invalid='ignore'):
+            deriv = np.log2(q[:-1,:-1]*q[1:,1:]) - \
+                    np.log2(q[:-1,1:]*q[1:,:-1]) + \
+                    np.log2(q[:-1,1:].sum(axis=2, keepdims=True) * \
+                            q[1:,:-1].sum(axis=2, keepdims=True)) - \
+                    np.log2(q[:-1,:-1].sum(axis=2, keepdims=True) * \
+                            q[1:,1:].sum(axis=2, keepdims=True))
 
         # get rid of nonsense values of deriv coming from finite numerical precision
-        deriv[deriv.isnan()] = 0
-        deriv[deriv.isnan()] = 0
+        deriv[np.logical_not(np.isfinite(deriv))] = 0
     
         # stores the coefficients of the q of the current
         # iteration. For each value of z there is a coeff, then
         # coeff_tot stacks all coeffs.
-        coeff_tot = np.zeros((dimx-1)*(dimy-1), dimz)
-    
+        coeff_tot = np.zeros(((dimx-1)*(dimy-1), dimz))
+
         for iz in range(dimz):
             
             # the constraints on q, for each z, are implemented via the inequality A*coeff<=b
-            b = np.zeros(2*dimx*dimy)
-            A = np.zeros(2*dimx*dimy, (dimx-1)*(dimy-1))
+            b = np.zeros((2*dimx*dimy,1))
+            A = np.zeros((2*dimx*dimy, (dimx-1)*(dimy-1)))
 
             # the number of constraints to be imposed
             count = 0
@@ -106,18 +109,18 @@ def lin_pid(p, accuracy=0.01, method='glpk'):
                         A[count, ix*(dimy-1)+iy] = -1
                         A[count+dimx*dimy, ix*(dimy-1)+iy] = 1
                         b[count] = p[ix+1,iy+1,iz]
-                        b[count+dimx*dimy] = 1-p[xx+1,yy+1,zz]
+                        b[count+dimx*dimy] = 1-p[ix+1,iy+1,iz]
                         count += 1
                         
-                    if xx==0 and yy==dimy-2:
+                    if ix==0 and iy==dimy-2:
                         # top right entries
                         A[count, ix*(dimy-1)+iy] = 1
                         A[count+dimx*dimy, ix*(dimy-1)+iy]=-1
                         b[count] = p[ix,iy+1,iz]
-                        b[count+ceil(dimx*dimy)) = 1-p[ix,iy+1,iz]
+                        b[count+dimx*dimy] = 1-p[ix,iy+1,iz]
                         count += 1
                         
-                    if xx==dimx-2 and iy==0:
+                    if ix==dimx-2 and iy==0:
                         # bottom left entries
                         A[count, ix*(dimy-1)+iy] = 1
                         A[count+dimx*dimy, ix*(dimy-1)+iy] = -1
@@ -150,7 +153,7 @@ def lin_pid(p, accuracy=0.01, method='glpk'):
                         A[count, ix*(dimy-1)+iy] = -1
                         A[count, (ix+1)*(dimy-1)+iy] = 1
                         A[count+dimx*dimy, ix*(dimy-1)+iy] = 1
-                        A[count+dimx*dimy, (ix+1)*(dimy-1)+iy) = -1
+                        A[count+dimx*dimy, (ix+1)*(dimy-1)+iy] = -1
                         b[count] = p[ix+1,iy+1,iz]
                         b[count+dimx*dimy] = 1-p[ix+1,iy+1,iz]
                         count +=1
@@ -173,68 +176,63 @@ def lin_pid(p, accuracy=0.01, method='glpk'):
 
             # extract the portion of deriv pertaining to iz and adapt
             # deriv_iz to the multiplication deriv_iz*coeff
-            deriv_zz = deriv[:,:,zz].reshape((-1,1), order='C')
+            deriv_iz = deriv[:,:,iz].reshape((-1,1), order='C')
 
             if method=='cvx':
                 raise NotImplementedError('CVX not yet implemented!')
             elif method=='glpk':
-                x = glpk.Variable(deriv_zz.size)
-                obj = glpk.Minimize(deriv_zz.T*x)
-                constraints = A*x<=b
-                prob = glpk.Problem(obj, constraints)
-                prob.solve(solver=glpk.GLPK, verbose=True)
+                x = cvxpy.Variable(deriv_iz.size)
+                obj = cvxpy.Minimize(deriv_iz.T*x)
+                constraints = [A*x<=b]
+                prob = cvxpy.Problem(obj, constraints)
+                prob.solve(verbose=verbose)
 
-            coeff_tot(:,iz) = prob.value
+            coeff_tot[:,iz] = prob.value
 
         coeff_tot = coeff_tot.reshape((-1,1), order='F')
 
 
         p_k = p[:]
-
-        for ind_gamma in range(dimz*(dimx-1)*(dimy-1)):
-            ix = np.mod(ind_gamma/(dimy-1), dimx-1) + 1
-            iy = np.mod(ind_gamma, dimy-1) + 1
-            iz = ind_gamma/((dimx-1)*(dimy-1))
-            p_k = p_k + coeff_tot(ind_gamma)*GAMMA(:,:,:,ix,iy,iz)
+        for ind_gamma in range((dimx-1)*(dimy-1)*dimz):
+            ix = np.mod(ind_gamma/(dimy-1), dimx-1).astype(np.int)
+            iy = np.mod(ind_gamma, dimy-1).astype(np.int)
+            iz = np.floor(ind_gamma/((dimx-1)*(dimy-1))).astype(np.int)
+            p_k = p_k + coeff_tot[ind_gamma]*GAMMA[:,:,:,ix,iy,iz]
       
         # this is supposed to build a concatenated array where the
-        # forst block corresponds to deriv_zz for zz=0, the second
-        # corresponds to deriv_zz for zz=1, etc
+        # forst block corresponds to deriv_iz for iz=0, the second
+        # corresponds to deriv_iz for iz=1, etc
         deriv = np.transpose(deriv, axes=(1,0,2)).reshape((-1,1), order='F')
         
         #set the stopping criterion based on the duality gap, see Stratos;
-        if iteration>0 and np.dot(deriv,coeff_prev-coeff_tot)<=accuracy:
+        if iteration>100:#iteration>0 and np.dot(deriv.T,coeff_prev-coeff_tot)<=accuracy:
+            print(iteration)
             done = True # exit the algorithm
-            q_opt = q # output the optimal distribution
         else:
             # fixed increment; simplest version of Franke-Wolf
-            gamma_k = 2/(iteationr+2)
+            gamma_k = 2/(iteration+2)
             # update the q for next iteration
             q = q + gamma_k*(p_k-q)
             # update coeff_prev for next i
             coeff_prev = coeff_prev + gamma_k*(coeff_tot-coeff_prev)
 
-
-        # update entropies
-        # TODO: no need to re-compute entropies at each step, one could just update the distribution
-        
-        # get rid of tiny negative entries in q ehich result from
+        iteration += 1
+            
+        # get rid of tiny negative entries in q which result from
         # limited numerical precision
         q[q<0] = 0
         
-        # compute I_q(X:Y)
-        I_xy = I_2D(q.sum(axis=2))
-
-        # compute I_q(X:Y|Z)
-        I_cond_xy_z = I_3D_cond_z(q)
-
-        # co-information
-        co_I = I_xy - I_cond_xy_z
 
 
-
-        
-    # get the output redundancy from the last coI_q and use it to compute the decomposition
+    # compute co-information for optimal distribution:
+    # coI_q(X:Y:Z)=I_q(X:Y)-I_q(X:Y|Z)
+    # co-information
+    co_I = I_2D(q.sum(axis=2)) - I_3D_cond_z(q)
+      
+    # compute the decomposition
+    #
+    # shared information is the coinformation of the optimal
+    # distribution
     SI = co_I
 
     I_xz = I_2D(p.sum(axis=1))
