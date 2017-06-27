9c83ab25880ee9090e7baa04dea8ac33653b156c
Eugenio Piasini <eugenio.piasini@iit.it>
2017-06-27 17:18:40 +0200

    More formatting and variable naming

--- src/matlab/split_PID_measures.m
+++ src/matlab/split_PID_measures.m
@@ -1,27 +1,25 @@
-function [Intersection_X,Intersection_Y,SR,MR]=split_PID_measures(p)
+function [RSI_x, RSI_y, SR_z, NSR_z] = split_PID_measures(p)
 
     % This code calculates the finer information-theoretic measures of the
-    % PID lattices of a system (X,Y,Z). Intersection_X is the information
-    % between Y and Z that passes through X; Intersection_Y is the
-    % information between X and Z that passes through Y; the source
-    % redundancy (SR) is the information about Z that is shared between X
-    % and Y and stored in the X-Y correlations; the mechanistic redundancy
-    % (MR) is information about Z that is shared between X and Y as it
-    % backfires to X and Y because there is a synergistic mechanism, or
-    % area, that combines X and Y to influence Z.
+    % PID lattices of a system (X,Y,Z). RSI_x is the information between Y
+    % and Z that passes through X (RSI(Z<X>Y)); RSI_y is the information
+    % between X and Z that passes through Y (RSI(Z<Y>X)); the source
+    % redundancy SR_z is the information about Z that is shared between X
+    % and Y and stored in the X-Y correlations; the non-source redundancy
+    % NSR_z is information about Z that does not arise from the
+    % correlations between the X and Y, and is related to the synergistic
+    % information that X and Y carry about Z.
     
     % All measures are output in bit.
     
     SI_z_xy = pid(p);
-    
     SI_x_yz = pid(permute(p,[3 2 1]));
-    
     SI_y_xz = pid(permute(p,[2 1 3]));
     
-    Intersection_Y=min(SI_z_xy,SI_x_yz);
-    Intersection_X=min(SI_z_xy,SI_y_xz);
+    RSI_y = min(SI_z_xy,SI_x_yz);
+    RSI_x = min(SI_z_xy,SI_y_xz);
     
-    SR=max(Intersection_X,Intersection_Y);
-    MR=SI_z_xy-SR;
+    SR_z = max(RSI_x,RSI_y);
+    NSR_z = SI_z_xy - SR_z;
 
 end
