41c69062f82b0e7dc2010fe0d5d76ba3494f6f24
Eugenio Piasini <eugenio.piasini@iit.it>
2017-10-02 10:07:06 +0200

    Fix forgotten optional argument check

--- src/matlab/qe_bias_correction_information.m
+++ src/matlab/qe_bias_correction_information.m
@@ -1,3 +1,6 @@
-function I = qe_bias_correction_information(X,Y, iters) 
+function I = qe_bias_correction_information(X,Y, iters)
+    if nargin<3
+        iters = 1;
+    end
     I = qe_bias_correction(X,1, iters) + qe_bias_correction(1,Y, iters) - qe_bias_correction(X,Y, iters);
 end
